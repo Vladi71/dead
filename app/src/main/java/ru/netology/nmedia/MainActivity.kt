b54067ccc5e17d5f3638944c23947583045ef3b3
Vladi71 <69647342+Vladi71@users.noreply.github.com>
2020-11-01 15:24:39 +0300

    mvvm commit

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,9 +2,13 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.observe
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.utils.Utils
+import ru.netology.nmedia.viewmodel.PostViewModel
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,44 +17,26 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(
-                id = 321,
-                author = "Нетология. Университет интернет-профессий будущего",
-                content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-                published = "21 мая в 18:36",
-                numberLike = 1099,
-                numberShare = 12,
-                numberViews = 999_999,
-                likeByMy = false
-        )
 
-
-        with(binding) {
-            textAuthor.text = post.author
-            textPublished.text = post.published
-            textContent.text = post.content
-            totalLikeView.text = Utils.formatTotal(post.numberLike)
-            totalViewsView.text = Utils.formatTotal(post.numberViews)
-
-            if (post.likeByMy) {
-                likeAvatarViews.setImageResource(R.drawable.red_like_avatars)
-            }
-            likeAvatarViews.setOnClickListener {
-                post.likeByMy = !post.likeByMy
-                if (post.likeByMy){
-                    likeAvatarViews.setImageResource(R.drawable.red_like_avatars)
-                    post.numberLike++
-                }else{
-                    likeAvatarViews.setImageResource(R.drawable.like_avatars)
-                    post.numberLike--
-
-                }
+        val viewModel: PostViewModel by viewModels()
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                textAuthor.text = post.author
+                textPublished.text = post.published
+                textContent.text = post.content
                 totalLikeView.text = Utils.formatTotal(post.numberLike)
-            }
-            viewsAvatarView.setOnClickListener{
-                post.numberViews++
                 totalViewsView.text = Utils.formatTotal(post.numberViews)
+                likeAvatarViews.setImageResource(
+                    if (post.likeByMy) R.drawable.red_like_avatars else R.drawable.like_avatars
+                )
             }
+        }
+        binding.likeAvatarViews.setOnClickListener {
+            viewModel.like()
+        }
+        binding.viewsAvatarView.setOnClickListener {
+            viewModel.view()
         }
     }
 }
+
--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -1,12 +1,16 @@
 package ru.netology.nmedia.dto
 
+import kotlinx.android.synthetic.main.activity_main.view.*
+import ru.netology.nmedia.R
+
 data class Post(
-    val id: Long,
-    val author: String,
-    val content: String,
-    val published: String,
-    var numberLike: Int,
-    var numberShare: Int,
-    var numberViews: Int,
-    var likeByMy: Boolean = false
+        val id: Long,
+        val avatar: Int = R.mipmap.ic_launcher,
+        val author: String,
+        val content: String,
+        val published: String,
+        val numberLike: Int,
+        val numberShare: Int,
+        val numberViews: Int,
+        val likeByMy: Boolean = false
 )
