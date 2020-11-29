353705d6cd825e45c27ba15598c0ec0234998d90
Vladi71 <RonRS6@mail.ru>
2020-11-29 21:24:46 +0300

    new json commit

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryFile.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryFile.kt
@@ -4,21 +4,31 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import ru.netology.nmedia.dto.Post
-import java.security.AccessControlContext
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
-class PostRepositoryFile(private val context: Context) : PostRepository {
-    private companion object{
-        const val FILE = "posts"
-        const val POSTS_KEY = "POSTS_KEY"
-
-    }
-    private var nextId = 1L
+class PostRepositoryFile(
+    private val context: Context
+) : PostRepository {
     private val gson = Gson()
-    private val preferences = context.getSharedPreferences(FILE, Context.MODE_PRIVATE)
+    private val type = TypeToken.getParameterized(List::class.java, Post::class.java).type
+    private val filename = "posts.json"
+    private var nextId = 1L
+    private var posts = emptyList<Post>()
+    private val mLData = MutableLiveData(posts)
 
-    private var posts = run {
-      val serialized = preferences.getString(POSTS_KEY, null) ?: return@run emptyList<Post>()
+    init {
+        val file = context.filesDir.resolve(filename)
+        if (file.exists()) {
+
+            context.openFileInput(filename).bufferedReader().use {
+                posts = gson.fromJson(it, type)
+                mLData.value = posts
+            }
+        } else {
+
+            sync()
+        }
     }
 
     private val data = MutableLiveData(posts)
@@ -28,17 +38,18 @@ class PostRepositoryFile(private val context: Context) : PostRepository {
         if (post.id == 0L) {
 
             posts = listOf(
-                    post.copy(
-                            id = nextId++,
-                            author = "Нетология. Университет интернет-профессий будущего",
-                            published = "09 ноября",
-                            likeByMy = false,
-                            numberLike = 0,
-                            numberViews = 0,
-                            numberShare = 0
-                    )
+                post.copy(
+                    id = nextId++,
+                    author = "Нетология. Университет интернет-профессий будущего",
+                    published = "29 ноября",
+                    likeByMy = false,
+                    numberLike = 0,
+                    numberViews = 0,
+                    numberShare = 0
+                )
             ) + posts
             data.value = posts
+            sync()
             return
         }
 
@@ -47,6 +58,7 @@ class PostRepositoryFile(private val context: Context) : PostRepository {
         }
 
         data.value = posts
+        sync()
     }
 
     override fun likeId(id: Long) {
@@ -55,12 +67,13 @@ class PostRepositoryFile(private val context: Context) : PostRepository {
                 it
             } else {
                 it.copy(
-                        likeByMy = !it.likeByMy, numberLike = if (it.likeByMy) it.numberLike - 1
-                else it.numberLike + 1
+                    likeByMy = !it.likeByMy, numberLike = if (it.likeByMy) it.numberLike - 1
+                    else it.numberLike + 1
                 )
             }
         }
         data.value = posts
+        sync()
     }
 
 
@@ -73,12 +86,14 @@ class PostRepositoryFile(private val context: Context) : PostRepository {
             }
         }
         data.value = posts
+        sync()
     }
 
 
     override fun removeById(id: Long) {
         posts = posts.filter { it.id != id }
         data.value = posts
+        sync()
     }
 
     override fun editById(id: Long, content: String) {
@@ -87,7 +102,13 @@ class PostRepositoryFile(private val context: Context) : PostRepository {
             set(index, posts[index].copy(content = content))
         }
         data.value = posts
+        sync()
     }
 
+    private fun sync() {
+        context.openFileOutput(filename, Context.MODE_PRIVATE).bufferedWriter().use {
+            it.write(gson.toJson(posts))
+        }
+    }
 }
 
